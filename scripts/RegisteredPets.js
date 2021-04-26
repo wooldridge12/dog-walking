0829c7917551d37f254df28e2d8a8ede0a6db7f7
wooldridge12 <wooldridgemikey@gmail.com>
2021-04-26 15:50:22 -0400

    Added whom is walking whom function

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -3,6 +3,46 @@ import { getPets, getWalkers } from "./database.js"
 const pets = getPets()
 const walkers = getWalkers()
 
+
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        if (clickEvent.target.id.startsWith("pet")) {
+            const [, petId] = clickEvent.target.id.split("--")
+            const petIdNumber = parseInt(petId)
+
+            const walkers = getWalkers()
+            const pets = getPets()
+
+            const foundPet = pets.find(
+                (pet) => {
+                    return pet.id === petIdNumber
+                }
+            )
+
+            const foundWalker = walkers.find(
+                (walker) => {
+                    return walker.id === foundPet.walkerId
+                }
+            ) 
+
+            window.alert(`${foundPet.name} is being walked by ${foundWalker.name}`)
+        }
+    }
+)
+
+
+
+
+
+
+
+
+
+    // intial attempt
+
 // document.addEventListener(
 //     "click",
 //     (clickEvent) => {
@@ -12,53 +52,64 @@ const walkers = getWalkers()
 
 //             for (const pet of pets) {
 //                 if (pet.id === parseInt(petId)) {
-//                     const petWalker = walkers.filter(
-//                         (walker) => {
-//                             if (walker.petId === pet.id) {
-//                                 return true
-//                             }
+//                     // let assignedWalker = {name:" "}
+                    
+
+//                     for (const walker of walkers) {
+//                         if (walker.id === parseInt(walkerId)) {
+
+                            
+//                             window.alert(`${pet.name} is being walked by ${walker.name}`)
 //                         }
-//                     )
-//                     window.alert(` ${pet.name} is being walked by ${petWalker.length}`)
+//                     }
 //                 }
 //             }
 //         }
 //     }
 // )
 
-document.addEventListener(
-    "click",
-    (clickEvent) => {
-        const itemClicked = clickEvent.target
-        if (itemClicked.id.startsWith("pet")) {
-            const [,petId] = itemClicked.id.split("--")
 
-            for (const pet of pets) {
-                if (pet.id === parseInt(petId)) {
-                    
-                    let assignedWalker = {name: ""};
 
-                    for (const walker of walkers) {
-                        if (walker.id === parseInt(walker.id)) {
-                            
-                        }
-                    }
-                    window.alert(`${pet.name} is being walked by ${assignedWalker.name}`)
-                }
-            }
-        }
-    }
-)
+    //Filter method
+
+// document.addEventListener(
+//     "click",
+//     (clickEvent) => {
+//         const itemClicked = clickEvent.target
+//         if (itemClicked.id.startsWith("pet")) {
+//             const[, petId] = itemClicked.id.split("--")
+
+//             for (const pet of pets) {
+//                 if (Pet.id === parseInt(petId)) {
+//                     const walkingWhom = walkers.filter(
+//                         (walker) => {
+//                             if(walker.petId === pet.id) {
+//                                 return true
+//                             }
+//                         }
+//                     )
+//                     window.alert(`${pet.name} is being walked by ${walkingWhom}`)
+//                 }
+//             }
+//         }
+//     }
+// )
+
+
+
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
+    // for (const walker of walkers) {
+    //     petHTML += `<li id="walker--${walker.id}">${walker.name}</li>`
+    // }
+
 
     petHTML += "</ul>"
-
     return petHTML
 }
 
