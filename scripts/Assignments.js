12cbdc3848d62b18ea74144be75f07531b5c4db6
wooldridge12 <wooldridgemikey@gmail.com>
2021-04-26 18:58:18 -0400

    fixing cities

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,8 +1,9 @@
-import { getPets, getWalkers } from "./database.js"
+import { getPets, getWalkers, getCities } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
+const cities = getCities()
 
 
 // Function whose responsibility is to find the walker assigned to a pet
@@ -22,12 +23,16 @@ export const Assignments = () => {
     let assignmentHTML = ""
      assignmentHTML = "<ul>"
 
+
     for (const currentPet of pets) {
         const currentPetWalker = findWalker(currentPet, walkers)
+    for (const city of cities) {
+        // assignmentHTML += `<li>${city.name}</li>`
+    // }
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${city.name}
             </li>
         `
     }
@@ -36,4 +41,4 @@ export const Assignments = () => {
 
     return assignmentHTML
 }
-
+}
--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,22 +1,56 @@
-import { getWalkers } from "./database.js"
+import { getWalkers, getCities} from "./database.js"
 
 const walkers = getWalkers()
+const cities = getCities()
+
 
 document.addEventListener(
     "click",
     (clickEvent) => {
-        const itemClicked = clickEvent.target
-        if (itemClicked.id.startsWith("walker")) {
-            const [,walkerId] = itemClicked.id.split("--")
-
-            for (const walker of walkers) {
-                if (walker.id === parseInt(walkerId)) {
-                    window.alert(`${walker.name} services ${walker.city}`)
+        if (clickEvent.target.id.startsWith("walker")) {
+            const [, walkerId] = clickEvent.target.id.split("--")
+            const walkerIdNumber = parseInt(walkerId)
+        
+            
+            const walkers = getWalkers()
+            const cities = getCities()
+            
+            const walkerName = walkers.find(
+                (walker) => {
+                    return walker.id === walkerIdNumber
                 }
-            }
+                )
+            const cityName = cities.find(
+                (city) => {
+                    return city.id === walkerName.cityId
+                }
+            )
+            window.alert(`${walkerName.name} services ${cityName.name}`)
         }
     }
 )
+
+
+
+
+
+
+            //Old code
+// document.addEventListener(
+//     "click",
+//     (clickEvent) => {
+//         const itemClicked = clickEvent.target
+//         if (itemClicked.id.startsWith("walker")) {
+//             const [,walkerId] = itemClicked.id.split("--")
+
+//             for (const walker of walkers) {
+//                 if (walker.id === parseInt(walkerId)) {
+//                     window.alert(`${walker.name} services ${walker.city}`)
+//                 }
+//             }
+//         }
+//     }
+// )
 
 
 
