7a998cd23d0c1d3cf1c13a3732178108bf695859
wooldridge12 <wooldridgemikey@gmail.com>
2021-04-26 14:09:39 -0400

    commiting

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,6 +1,54 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
+
+// document.addEventListener(
+//     "click",
+//     (clickEvent) => {
+//         const itemClicked = clickEvent.target
+//         if (itemClicked.id.startsWith("pet")) {
+//             const [, petId] = itemClicked.id.split("--")
+
+//             for (const pet of pets) {
+//                 if (pet.id === parseInt(petId)) {
+//                     const petWalker = walkers.filter(
+//                         (walker) => {
+//                             if (walker.petId === pet.id) {
+//                                 return true
+//                             }
+//                         }
+//                     )
+//                     window.alert(` ${pet.name} is being walked by ${petWalker.length}`)
+//                 }
+//             }
+//         }
+//     }
+// )
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [,petId] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+                    
+                    let assignedWalker = {name: ""};
+
+                    for (const walker of walkers) {
+                        if (walker.id === parseInt(walker.id)) {
+                            
+                        }
+                    }
+                    window.alert(`${pet.name} is being walked by ${assignedWalker.name}`)
+                }
+            }
+        }
+    }
+)
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
